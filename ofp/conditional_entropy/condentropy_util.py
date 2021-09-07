a1f807bed397259001a73330da53d8a9eb666893
ncsa-mo <ondrejce@illinois.edu>
2021-09-07 14:55:12 -0500

    Add files via upload

---
+++ ofp/conditional_entropy/condentropy_util.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+class CondEntropyUtil:
+    """ Pre-processing functions. """
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def word_split(text):
+        punctuation = '.,():-—;"!?•$%@“”#<>+=/[]*^\'{}_■~\\|«»©&~`£·'
+        text = text.replace('-', ' ')
+        # we replace hyphens with spaces because it seems probable that for this purpose
+        # we want to count hyphen-divided phrases as separate words
+        word_seq = [x.strip(punctuation).lower() for x in text.split()]
+
+        return word_seq
+
+
+
+
+
+
+
+
+
+
+
+
+
